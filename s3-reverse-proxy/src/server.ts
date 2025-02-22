fa16518178a2198c40d78e71a9ae9bb732009b5e
ManavChaudhary999 <999manavchaudhary@gmail.com>
2025-02-22 18:27:51 +0530

    Subdomain to Project id map in reverse-proxy

--- s3-reverse-proxy/src/server.ts
+++ s3-reverse-proxy/src/server.ts
@@ -1,5 +1,6 @@
-const express = require("express");
-const httpProxy = require('http-proxy');
+import express from 'express';
+import httpProxy from 'http-proxy';
+import db from './db';
 
 const app = express();
 var proxy = httpProxy.createProxyServer({});
@@ -7,12 +8,18 @@ var proxy = httpProxy.createProxyServer({});
 const PORT = 8000;
 const BASE_URL = 'https://vercel-clone-999.s3.eu-north-1.amazonaws.com/__outputs';
 
-app.use((req, res) => {
+app.use(async (req, res) => {
     const hostname = req.hostname;
     const subdomain = hostname.split('.')[0];
 
+    const project = await db.project.findFirst({
+        where: {
+            subdomain: subdomain
+        }
+    });
+
     proxy.web(req, res, {
-        target: `${BASE_URL}/${subdomain}`,
+        target: `${BASE_URL}/${project?.id}`,
         changeOrigin: true,
     });
 });
@@ -25,3 +32,10 @@ proxy.on('proxyReq', (proxyReq, req, res) => {
 app.listen(PORT, () => {
     console.log(`Reverse Proxy running on port: ${PORT}`);
 });
+
+async function init() {
+    const projects = await db.project.findMany();
+    console.log(projects);
+}
+
+init();
